06578bec815791b114a22670208b6656a631de64
mns2324 <mnsbartolata@addu.edu.ph>
2026-01-15 09:53:25 +0800

    Created Agarthan Function to keep the T50s away from HyperBorea -Yakub

--- wwwroot/subjects.py
+++ wwwroot/subjects.py
@@ -27,7 +27,7 @@ try:
     # allow execution of sql queries
     cursor = conn.cursor()
 
-    # insert, update, delete 
+    # insert, update, delete to sql
     if action == "insert" and subjcode and subjdesc and subjunits and subjsched:
         cursor.execute(
             "INSERT INTO subjects (subjcode, subjdesc, subjunits, subjsched) VALUES (%s, %s, %s, %s)",
@@ -37,7 +37,7 @@ try:
 
     elif action == "update" and subjid and subjcode and subjdesc and subjunits and subjsched:
         cursor.execute(
-            "UPDATE subjects SET subjcode=%s, subjdesc=%s, subjunits=%s, subjunits=%s WHERE subjid=%s",
+            "UPDATE subjects SET subjcode=%s, subjdesc=%s, subjunits=%s, subjsched=%s WHERE subjid=%s",
             (subjcode, subjdesc, subjunits, subjsched, subjid)
         )
         conn.commit()
@@ -46,22 +46,26 @@ try:
         cursor.execute( "DELETE FROM subjects WHERE subjid=%s", (subjid,) )
         conn.commit()
 
-    # read all records from users
+    # read all records from subjects
     cursor.execute("SELECT subjid, subjcode, subjdesc, subjunits, subjsched FROM subjects")
     rows = cursor.fetchall()
 
     print("""
     <html>
-    <script>
-    // copies data into the input fields to allow updating
-    function fillForm(subjid, subjcode, subjdesc, subjunits, subjsched) {
-        document.getElementById("subjid").value = subjid;
-        document.getElementById("subjcode").value = subjcode;
-        document.getElementById("subjdesc").value = subjdesc;
-        document.getElementById("subjunits").value = subjunits;
-        document.getElementById("subjsched").value = subjsched;
-    }
-    </script>
+    <head>
+        <style>
+        body {
+            background-color: #000000;
+            color: white;
+        }
+        input {
+            background-color: #000000;
+            color: white;
+        }
+        </style>
+        
+        
+    </head>
 
     <body>
     <table width="100%" cellpadding="10">
@@ -74,7 +78,7 @@ try:
             <td width="30%" valign="top">
                 <h3>Subjects Form</h3>
                 <!-- submit data back to this script -->
-                <form action="subjects.py" method="post">
+                <form id="hello" action="subjects.py" method="post">
                     Subject ID:<br>
                     <input type="text" name="subjid" id="subjid"><br>
                     Subject Code:<br>
@@ -82,11 +86,12 @@ try:
                     Description:<br>
                     <input type="text" name="subjdesc" id="subjdesc"><br><br>
                     Units:<br>
-                    <input type="text" name="subjunits" id="subjunits"><br><br>
+                    <input type="number" name="subjunits" id="subjunits"><br><br>
                     Schedule:<br>
                     <input type="text" name="subjsched" id="subjsched"><br><br>
 
-                    <input type="hidden" name="action" id="action">
+                    <!-- use ts to append the subjid to the url, somehow -->
+                    <input type="hidden" name="action" id="subjappend">
                   
                     <input type="submit" value="Insert" onclick="document.getElementById('action').value='insert'">
                     <input type="submit" value="Update" onclick="document.getElementById('action').value='update'">
@@ -109,17 +114,24 @@ try:
 
     # clicking a row fills the form fields/input boxes
     for i in range(len(rows)):
-        name_val = html.escape(rows[i][0])
-        age_val = str(rows[i][1])
-        email_val = html.escape(str(rows[i][2]))
+        subjid = str(rows[i][0])
+        subjcode = html.escape(str(rows[i][1]))
+        subjdesc = html.escape(str(rows[i][2]))
+        subjunits = str(rows[i][3])
+        subjsched = html.escape(str(rows[i][4]))
+
+        urlsubjappend = str(rows[i][0])
 
         print(
-            "<tr onclick=\"fillForm('{}','{}','{}')\" style=\"cursor:pointer;\">"
-            .format(name_val, age_val, email_val)
+            "<tr onclick=\"fillForm('{}','{}','{}','{}','{}','{}')\" style=\"cursor:pointer;\">"
+            .format(subjid, subjcode, subjdesc, subjunits, subjsched, urlsubjappend)
         )
-        print("<td>" + name_val + "</td>")
-        print("<td>" + age_val + "</td>")
-        print("<td>" + email_val + "</td>")
+        print("<td>" + subjid + "</td>")
+        print("<td>" + subjcode + "</td>")
+        print("<td>" + subjdesc + "</td>")
+        print("<td>" + subjunits + "</td>")
+        print("<td>" + subjsched + "</td>")
+        print("<td>0</td>") # total units here
         print("</tr>")
 
     print("""
@@ -129,7 +141,8 @@ try:
         
         <tr>
             <td width="30%"></td> <!-- empty cell to align with form -->
-                <h3>Students Enrolled in Subject ID (student id)</h3> <!-- use javascript to change this i think -->
+            <td width="70%" valign="top">
+                <h3 id="changesubjid">Students Enrolled in Subject ID: (not selected yet)</h3> <!-- use javascript to change this i think -->
                 <table border="1" cellpadding="5" cellspacing="0" width="100%">
                     <tr>
                         <th>ID</th>
@@ -144,6 +157,23 @@ try:
         </tr>
     </table>
     </body>
+    <script>
+        const changethisid = document.getElementById('changesubjid');
+    
+        // copies data into the input fields to allow updating
+        function fillForm(subjid, subjcode, subjdesc, subjunits, subjsched) {
+            document.getElementById("subjid").value = subjid;
+            document.getElementById("subjcode").value = subjcode;
+            document.getElementById("subjdesc").value = subjdesc;
+            document.getElementById("subjunits").value = subjunits;
+            document.getElementById("subjsched").value = subjsched;
+            document.getElementById("changesubjid").innerText = "Students Enrolled in Subject ID: " + subjid;
+
+            // window.location.href = `subjects.py/?subjid=${subjid}`
+            boob = window.location.href + `?
+        }
+
+        </script>
     </html>
     """)
 
