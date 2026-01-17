b27fbe2ee40a7a0004a223459764b9dbd205755b
mns2324 <mnsbartolata@addu.edu.ph>
2026-01-17 17:39:33 +0800

    Made the "Students enrolled in Subject ID: " header changed based on the selected Subject ID
    Missing: Show the students currently enrolled in the selected subject.

--- wwwroot/subjects.py
+++ wwwroot/subjects.py
@@ -10,6 +10,8 @@ form = cgi.FieldStorage()
 action = form.getvalue("action", "")
 
 subjid = form.getvalue("subjid", "")
+selected_subjid = form.getvalue("subjid")
+
 subjcode = html.escape(form.getvalue("subjcode", ""))
 subjdesc = html.escape(form.getvalue("subjdesc", ""))
 subjunits = html.escape(form.getvalue("subjunits", ""))
@@ -49,6 +51,12 @@ try:
     # read all records from subjects
     cursor.execute("SELECT subjid, subjcode, subjdesc, subjunits, subjsched FROM subjects")
     rows = cursor.fetchall()
+    
+    # show what subject id is currently selected
+    if selected_subjid:
+        heading = f"Students Enrolled in Subject ID: {html.escape(selected_subjid)}"
+    else:
+        heading = "Students Enrolled in Subject ID: (not selected yet)"
 
     print("""
     <html>
@@ -62,16 +70,53 @@ try:
             background-color: #000000;
             color: white;
         }
+        table { 
+            border-collapse:collapse; 
+        }
+        th, td { 
+            border:1px solid white; padding:5px; 
+        }
         </style>
         
+        <script>  
         
+        // copies data into the input fields to allow updating
+        function fillForm(subjid, subjcode, subjdesc, subjunits, subjsched) {
+            document.getElementById("subjid").value = subjid;
+            document.getElementById("subjcode").value = subjcode;
+            document.getElementById("subjdesc").value = subjdesc;
+            document.getElementById("subjunits").value = subjunits;
+            document.getElementById("subjsched").value = subjsched;
+            document.getElementById("changesubjid").innerText = "Students Enrolled in Subject ID: " + subjid;
+
+            // pushState should make the page not reload compared to window.location.href
+            history.pushState({}, "", `subjects.py?subjid=${subjid}`);
+            
+            updateStudentUrl();
+        }
+            
+        function updateStudentUrl() {
+            // grab the subjects url, get the current subjid, then append it to the students href
+            const params = new URLSearchParams(window.location.search);
+            const subjid = params.get("subjid");
+            const link = document.getElementById("studentformurl");
+            
+            if (subjid) {
+                link.href = `http://localhost/students.py?subjid=${subjid}`;
+            }
+        }
+        
+        // run this function when the subjects form is loaded
+        window.addEventListener("load", updateStudentUrl);
+        
+        </script>        
     </head>
 
     <body>
     <table width="100%" cellpadding="10">
         <tr>
             <td colspan="2">
-                <a href="http://localhost/students.py">Students</a>
+                <a id="studentformurl" href="http://localhost/students.py">Students</a>
             </td>
         </tr>
         <tr>
@@ -80,7 +125,7 @@ try:
                 <!-- submit data back to this script -->
                 <form id="hello" action="subjects.py" method="post">
                     Subject ID:<br>
-                    <input type="text" name="subjid" id="subjid"><br>
+                    <input type="text" name="subjid" id="subjid" readonly><br>
                     Subject Code:<br>
                     <input type="text" name="subjcode" id="subjcode"><br>
                     Description:<br>
@@ -91,7 +136,7 @@ try:
                     <input type="text" name="subjsched" id="subjsched"><br><br>
 
                     <!-- use ts to append the subjid to the url, somehow -->
-                    <input type="hidden" name="action" id="subjappend">
+                    <input type="hidden" name="action" id="action">
                   
                     <input type="submit" value="Insert" onclick="document.getElementById('action').value='insert'">
                     <input type="submit" value="Update" onclick="document.getElementById('action').value='update'">
@@ -123,8 +168,8 @@ try:
         urlsubjappend = str(rows[i][0])
 
         print(
-            "<tr onclick=\"fillForm('{}','{}','{}','{}','{}','{}')\" style=\"cursor:pointer;\">"
-            .format(subjid, subjcode, subjdesc, subjunits, subjsched, urlsubjappend)
+            "<tr onclick=\"fillForm('{}','{}','{}','{}','{}')\" style=\"cursor:pointer;\">"
+            .format(subjid, subjcode, subjdesc, subjunits, subjsched)
         )
         print("<td>" + subjid + "</td>")
         print("<td>" + subjcode + "</td>")
@@ -142,7 +187,7 @@ try:
         <tr>
             <td width="30%"></td> <!-- empty cell to align with form -->
             <td width="70%" valign="top">
-                <h3 id="changesubjid">Students Enrolled in Subject ID: (not selected yet)</h3> <!-- use javascript to change this i think -->
+                <h3 id="changesubjid">""" + heading + """</h3>
                 <table border="1" cellpadding="5" cellspacing="0" width="100%">
                     <tr>
                         <th>ID</th>
@@ -157,23 +202,6 @@ try:
         </tr>
     </table>
     </body>
-    <script>
-        const changethisid = document.getElementById('changesubjid');
-    
-        // copies data into the input fields to allow updating
-        function fillForm(subjid, subjcode, subjdesc, subjunits, subjsched) {
-            document.getElementById("subjid").value = subjid;
-            document.getElementById("subjcode").value = subjcode;
-            document.getElementById("subjdesc").value = subjdesc;
-            document.getElementById("subjunits").value = subjunits;
-            document.getElementById("subjsched").value = subjsched;
-            document.getElementById("changesubjid").innerText = "Students Enrolled in Subject ID: " + subjid;
-
-            // window.location.href = `subjects.py/?subjid=${subjid}`
-            boob = window.location.href + `?
-        }
-
-        </script>
     </html>
     """)
 
